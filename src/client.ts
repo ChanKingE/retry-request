8b87fcb11929935d8243471b0f4e2ca7f95c22dc
chan <839831268@qq.com>
2026-09-19 09:19:57 +0800

    chore: 移除client.ts中多余的initialConfig打印日志

--- src/client.ts
+++ src/client.ts
@@ -205,7 +205,6 @@ export class RequestClient {
     }
 
     const initialConfig = this.#applyDefaults(config);
-    console.log(initialConfig);
 
     try {
       const interceptedConfig = await this.#requestInterceptors.run(initialConfig);
